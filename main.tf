c499f5c4a4a8b3603a0db2aacc340664c49db83a
shefeekar <shefeekar2@gmail.com>
2024-01-19 18:31:11 +0530

    add aws provider block and create endpoint for testing purpose using local stack

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.33.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key    = "AKIAQEARDRG4X7OFXVQC"
+  secret_key                  = "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"
+  region                      = "us-east-1"
+ 
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints{
+      s3             = "http://s3.localhost.localstack.cloud:4566"
+  }
+}
